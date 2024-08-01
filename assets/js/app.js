5c6d09a6d1dda1eb4d2d626384edf88851db732b
Nabin Singh <nabin@skilluplabs.com.au>
2024-08-01 20:14:03 +1000

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,8 +10,9 @@ function populateLabels() {
     });
     document.querySelector('.labels-wrap').innerHTML = labelElements.join("");
     document.querySelectorAll('.label-btn').forEach(button => {
-        button.addEventListener('click', (event) => {
-            const label = event.target.textContent;
+        button.addEventListener('click', async (event) => {
+            const query = event.target.textContent;
+            await searchImages(query);
         })
     })
 }
@@ -22,6 +23,10 @@ document.querySelector('form').addEventListener('submit', async function(event) 
     event.preventDefault();
 
     const query = document.querySelector("#search").value;
+    await searchImages(query);
+});
+
+async function  searchImages(query) {
     console.log({query});
     const url = `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${ACCESS_KEY}`;
     console.log({url})
@@ -37,4 +42,4 @@ document.querySelector('form').addEventListener('submit', async function(event) 
         console.log({imageUrl});
     }
     document.querySelector('.images').innerHTML = imageElements;
-})
+}
